5c0594363905ce68e4a064c442bd48986b8b0b22
Ian Preston <ian@ian-preston.com>
2012-11-22 01:06:37 -0700

    add placeholder intro text to the homepage

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,19 @@
 $(function() {
     $('.terms-searchbox').keyup(function() {
-        $('.terms-container').load('/terms/search?q=' + encodeURIComponent($('.terms-searchbox').val()));
+        var query = $('.terms-searchbox').val();
+        $('.terms-container').load('/terms/search?q=' + encodeURIComponent(query));
+
+        if (query.length > 0 && $('.intro-text').is(':hidden') == false) {
+            $('.terms-container').show();
+            $('.intro-text').fadeOut();
+        }
+        if (query.length == 0 && $('.intro-text').is(':hidden')) {
+            $('.terms-container').hide();
+            $('.intro-text').fadeIn();
+        }
+    });
+
+    $('.intro-text').fadeIn(1500, function() {
+        $('.terms-searchbox').focus();
     });
 });
